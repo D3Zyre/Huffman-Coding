8482a5261183572a4ad56472bbe379fdd3030743
D3Zyre <onebitalpha@gmail.com>
2022-11-19 14:02:39 -0700

    made some attributes and methods private, get/set

--- Huffman_tree.py
+++ Huffman_tree.py
@@ -1,4 +1,7 @@
 def justify_to_8(num):
+    """
+    return the lowest multiple of 8 that is greater than or equal to num
+    """
     just = int(num//8)*8+8
     if num % 8 == 0:
         just -= 8
@@ -6,46 +9,75 @@ def justify_to_8(num):
 
 class Node:
     def __init__(self, string_to_encode: str = None, value: str = None, count: int = None, left_node=None, right_node=None):
-        self.value = value  # value that the node encodes, a single character
-        self.char_counts = dict()  # only the top node has a non-empty dict, {char : count, ...}
-        self.depth = 0  # gets fixed by self.shift_depth()
-        self.count = count  # number of characters in tree (Node + all subNodes)
-        self.left_child = left_node  # Node object for left Node, or None
-        self.right_child = right_node  # Node object for right Node, or None
-        self.string = string_to_encode  # only top Node has this
-        self.code_string = str()  # the encoded binary string representation of self.string
-        self.encoding_dict = dict()
+        self.__value = value  # value that the node encodes, a single character
+        self.__char_counts = dict()  # only the top node has a non-empty dict, {char : count, ...}
+        self.__depth = 0  # gets fixed by self.__shift_depth()
+        self.__count = count  # number of characters in tree (Node + all subNodes)
+        self.__left_child = left_node  # Node object for left Node, or None
+        self.__right_child = right_node  # Node object for right Node, or None
+        self.__string = string_to_encode  # only top Node has this
+        self.__code_string = str()  # the encoded binary string representation of self.__string
+        self.__encoding_dict = dict()
         if string_to_encode is not None:
-            self.build_tree()  # only build tree from top level
-            self.shift_depth()  # assigns the correct value to each Node's depth
-            self.encoding_dict = self.create_encoding()
-            self.encode_string()
+            self.__build_tree()  # only build tree from top level
+            self.__shift_depth()  # assigns the correct value to each Node's depth
+            self.__encoding_dict = self.__create_encoding()
+            self.__encode_string()
 
-    def shift_depth(self, shift: int = 0):
+    def set_string(self, string_to_encode: str):
+        """
+        changes the string and rebuilds tree and all
+        """
+        self.__string = string_to_encode
+        self.__build_tree()
+        self.__shift_depth()
+        self.__encoding_dict = self.__create_encoding()
+        self.__encode_string()
+    
+    def get_string(self):
+        """
+        returns self.__string
+        """
+        return self.__string
+    
+    def get_huffman_code(self):
+        """
+        returns self.__code_string
+        """
+        return self.__code_string
+    
+    def get_encoding_dict(self):
+        """
+        returns the huffman encoding pairs as a dict
+        self.__encoding_dict
+        """
+        return self.__encoding_dict
+
+    def __shift_depth(self, shift: int = 0):
         """
         shifts depth values for Node
         and all subnodes such that
         top Node depth is 0, all depths
         will be positive
         """
-        self.depth += shift
-        self.left_child: Node
-        self.right_child: Node
+        self.__depth += shift
+        self.__left_child: Node
+        self.__right_child: Node
         # recursively add the depth to all children
-        if self.left_child is not None:
-            self.left_child.shift_depth(shift + 1)
-        if self.right_child is not None:
-            self.right_child.shift_depth(shift + 1)
+        if self.__left_child is not None:
+            self.__left_child.__shift_depth(shift + 1)
+        if self.__right_child is not None:
+            self.__right_child.__shift_depth(shift + 1)
 
-    def build_tree(self):
+    def __build_tree(self):
         """
         builds the Huffman tree inside
         the current node object
         """
-        self.count_chars()  # we will need the dictionary for building the tree
+        self.__count_chars()  # we will need the dictionary for building the tree
         nodes = list()  # a list of Node objects
-        for leaf in self.char_counts.keys():
-            nodes.append(Node(None, leaf, self.char_counts[leaf]))
+        for leaf in self.__char_counts.keys():
+            nodes.append(Node(None, leaf, self.__char_counts[leaf]))
         # all children have been created (Nodes containing each character and its count)
         while len(nodes) > 1:  # repeat until there is only one tree
             # combine two smallest branches:
@@ -62,32 +94,32 @@ class Node:
             nodes.append(Node(None, None, int(sum([leaf.count for leaf in picks])), picks[0], picks[1]))
             # new branch has been added, and leaves were moved to it
         # updating top node's attributes, essentially copying nodes[0] into self
-        self.count = nodes[0].count
-        nodes[0].string = self.string
-        nodes[0].count_chars()
-        self.char_counts = nodes[0].char_counts
-        self.depth = nodes[0].depth
-        self.left_child = nodes[0].left_child
-        self.right_child = nodes[0].right_child
+        self.__count = nodes[0].count
+        nodes[0].string = self.__string
+        nodes[0].__count_chars()
+        self.__char_counts = nodes[0].char_counts
+        self.__depth = nodes[0].depth
+        self.__left_child = nodes[0].left_child
+        self.__right_child = nodes[0].right_child
 
-    def count_chars(self):
+    def __count_chars(self):
         """
-        counts unique characters in self.string
+        counts unique characters in self.__string
         creates a dictionary with characters as keys
-        and counts as values, self.counts
+        and counts as values, self.__counts
         """
-        self.char_counts = dict()  # reset dict to be empty
-        for character in self.string:
-            if character in self.char_counts.keys():
-                self.char_counts[character] += 1
+        self.__char_counts = dict()  # reset dict to be empty
+        for character in self.__string:
+            if character in self.__char_counts.keys():
+                self.__char_counts[character] += 1
             else:
-                self.char_counts[character] = 1
-        self.char_counts["EOF"] = 1
+                self.__char_counts[character] = 1
+        self.__char_counts["EOF"] = 1
 
-    def create_encoding(self, is_left: bool = None, is_right: bool = None, huffman_code: str = None):
+    def __create_encoding(self, is_left: bool = None, is_right: bool = None, huffman_code: str = None):
         """
         creates a dictionary for the encoding of
-        self.string with the huffman coding represented
+        self.__string with the huffman coding represented
         by the tree in self Node object (children and their children)
         """
         assert (is_left is True and is_right is False) or (
@@ -105,46 +137,46 @@ class Node:
             huffman_code = str()  # set to empty string
         else:
             huffman_code += current_pos_code
-        if self.value is not None:  # if we are on a leaf node
-            encoding_dict[self.value] = huffman_code
-        if self.left_child is not None:
-            dict_updates: dict = self.left_child.create_encoding(is_left=True, is_right=False, huffman_code=huffman_code)
+        if self.__value is not None:  # if we are on a leaf node
+            encoding_dict[self.__value] = huffman_code
+        if self.__left_child is not None:
+            dict_updates: dict = self.__left_child.__create_encoding(is_left=True, is_right=False, huffman_code=huffman_code)
             for key in [k for k in dict_updates.keys() if k not in encoding_dict.keys()]:
                 # only update values which were not already in the dict
                 encoding_dict[key] = dict_updates[key]  # update encoding dict
-        if self.right_child is not None:
-            dict_updates: dict = self.right_child.create_encoding(is_left=False, is_right=True, huffman_code=huffman_code)
+        if self.__right_child is not None:
+            dict_updates: dict = self.__right_child.__create_encoding(is_left=False, is_right=True, huffman_code=huffman_code)
             for key in [k for k in dict_updates.keys() if k not in encoding_dict.keys()]:
                 # only update values which were not already in the dict
                 encoding_dict[key] = dict_updates[key]  # update encoding dict
 
         return encoding_dict  # to be updated in upper recursion levels, and final version returned to caller
 
-    def encode_string(self):
+    def __encode_string(self):
         """
-        uses self.encoding_dict and self.string
-        in the top node to encode the string to self.code_string
+        uses self.__encoding_dict and self.__string
+        in the top node to encode the string to self.__code_string
         """
-        assert (self.string is not None), "encode_string is only a method of the top node of a tree"
-        assert (self.string.count("\0") == 0), "NULL character not allowed in string"
+        assert (self.__string is not None), "__encode_string is only a method of the top node of a tree"
+        assert (self.__string.count("\0") == 0), "NULL character not allowed in string"
         huffman_code = str()
-        for char in self.string:
-            huffman_code += str(self.encoding_dict[char])
-        huffman_code += str(self.encoding_dict["EOF"])
-        self.code_string = str(huffman_code)
+        for char in self.__string:
+            huffman_code += str(self.__encoding_dict[char])
+        huffman_code += str(self.__encoding_dict["EOF"])
+        self.__code_string = str(huffman_code)
 
     def decode_string(self):
         """
-        uses self.encoding_dict and self.code_string
-        in the top node to decode the string to self.string
+        uses self.__encoding_dict and self.__code_string
+        in the top node to decode the string to self.__string
         """
-        assert (len(self.code_string) > 0), "decode_string only works when self.code_string is not empty string"
+        assert (len(self.__code_string) > 0), "decode_string only works when self.__code_string is not empty string"
         string = str()
         digit_run = str()  # the run of digits since last run was identified
-        huff_codes = list(self.encoding_dict.values())  # so we don't have to call this a bunch of times
-        huff_keys = list(self.encoding_dict.keys())
+        huff_codes = list(self.__encoding_dict.values())  # so we don't have to call this a bunch of times
+        huff_keys = list(self.__encoding_dict.keys())
         addition = str()
-        for digit in self.code_string:
+        for digit in self.__code_string:
             digit_run += digit  # keep adding digits until a valid huffman code is found
             if digit_run in huff_codes:
                 addition = huff_keys[huff_codes.index(digit_run)]
@@ -153,7 +185,7 @@ class Node:
                 digit_run = str()  # since we have found the code, we reset the run
             if addition == "EOF":
                 break
-        self.string = str(string)
+        self.__string = str(string)
 
     def write_to_file(self, file):
         """
@@ -183,8 +215,8 @@ class Node:
         # well, it appears that for this specific example at least, both formats are equivalent in size
         # for simplicity, let's use option 2, as it is simple to encode and decode
         total_length = int()
-        for key in self.encoding_dict.keys():
-            binary_string += str(key) + str(self.encoding_dict[key])
+        for key in self.__encoding_dict.keys():
+            binary_string += str(key) + str(self.__encoding_dict[key])
         binary_string += "\0\n"  # add NULL and newline to signify the end of the tree (NULL isn't allowed in the string)
         total_length += len(binary_string)
         # this part of the string, we will want to write in text mode,
@@ -193,13 +225,13 @@ class Node:
             f.write(binary_string)
         with open(file, "ab") as f:  # appends to the same file, now in binary mode
             b_array = list()
-            code_string = self.code_string.ljust(justify_to_8(len(self.code_string)), "0")  # pad with 0s at the end to encode as bytes
+            code_string = self.__code_string.ljust(justify_to_8(len(self.__code_string)), "0")  # pad with 0s at the end to encode as bytes
             for i in range(0, len(code_string), 8):
                 b_array.append(int(code_string[i:i+8], base=2))
             binary_string = bytearray(b_array)
             f.write(binary_string)
         total_length += len(binary_string)
-        efficiency = total_length/len(self.string)
+        efficiency = total_length/len(self.__string)
 
         return efficiency
 
@@ -234,32 +266,32 @@ class Node:
                 last_char_number = True
                 current_number += char
         tree_dict[current_string] = current_number  # add the last entry
-        # now tree_dict will match exactly the self.encoding_dict that was used to encode
+        # now tree_dict will match exactly the self.__encoding_dict that was used to encode
         binary_string = bin(int.from_bytes(encoded_string, "big"))[2:]
         binary_string = binary_string.rjust(justify_to_8(len(binary_string)), "0")
         # update attributes in Node and decode string
-        self.code_string = binary_string
-        self.encoding_dict = tree_dict
+        self.__code_string = binary_string
+        self.__encoding_dict = tree_dict
         self.decode_string()
 
     def __str__(self):
         output = str((
-            "\t"*self.depth + "string = {}\n" +
-            "\t"*self.depth + "encoding = {}\n" +
-            "\t"*self.depth + "value = {}\n" +
-            "\t"*self.depth + "char counts = {}\n" +
-            "\t"*self.depth + "depth = {}\n" +
-            "\t"*self.depth + "count = {}\n" +
-            "\t"*self.depth + "left child = [{}]\n" +
-            "\t"*self.depth + "right child = [{}]\n").format(
-                    self.string,
-                    self.encoding_dict,
-                    self.value,
-                    self.char_counts,
-                    self.depth,
-                    self.count,
-                    str(self.left_child),
-                    str(self.right_child)
+            "\t"*self.__depth + "string = {}\n" +
+            "\t"*self.__depth + "encoding = {}\n" +
+            "\t"*self.__depth + "value = {}\n" +
+            "\t"*self.__depth + "char counts = {}\n" +
+            "\t"*self.__depth + "depth = {}\n" +
+            "\t"*self.__depth + "count = {}\n" +
+            "\t"*self.__depth + "left child = [{}]\n" +
+            "\t"*self.__depth + "right child = [{}]\n").format(
+                    self.__string,
+                    self.__encoding_dict,
+                    self.__value,
+                    self.__char_counts,
+                    self.__depth,
+                    self.__count,
+                    str(self.__left_child),
+                    str(self.__right_child)
                     )
                 )
         # by printing left/right child, we will call __str__ on them,
@@ -269,10 +301,11 @@ class Node:
 
 
 if __name__ == "__main__":
-    a = Node("""hello world! this is a very long string, let's add a few special characters as well for fun: !@#$%^&*()""")
+    a = Node("""hello world! this is a very long string,
+    let's add a few special characters as well for fun: !@#$%^&*()""")
     b = Node("hello world!")
-    c = Node("test\ntesting\ntesteroo\n\ttesterino")
-    z = c
+    c = Node("test\ntesting\ntesteroo\ntesterino")
+    z = a
     print(z.string)  # before decoding, original string
     #print(z.encoding_dict)
     #print(z.code_string)
